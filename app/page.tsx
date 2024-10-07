a6a0d37e952e98ff9c108594085efa96db5a7e9e
AyanAhmedKhan381 <Sakratsuperstar@gmail.com>
2024-10-07 21:10:08 +0500

    Degital clock

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import DigitalClock from "@/components/ui/digital-clock";
+// import Footer from "@/components/ui/footer";
+
+export default function Home() {
+  return (
+    <div>
+      <DigitalClock />
+      {/* <Footer/> */}
+    </div>
+  );
+}
---
+++ components/ui/footer.tsx
@@ -0,0 +1,14 @@
+
+
+// import React from 'react'
+
+// const Footer = () => {
+//   return (
+//     <div className='mt-10 mb-10 text-2xl bg-black p-5'>
+//       <h1 className='text-white'><span className='text-red-500'>Made by</span>
+//       <span className='text-orange-500'>AyanDev</span>Solutions</h1>
+//     </div>
+//   )
+// }
+
+// export default Footer
